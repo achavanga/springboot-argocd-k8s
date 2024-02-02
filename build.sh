65290d1bca19c976a744d349a764c29ce50be6c6
Abel Chavanga <achavanga@bol.com>
2024-02-02 20:54:55 +0100

    Fix docker images

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 docker build -t spring-argocd-app:v1 .
 
-docker tag spring-argocd-app:v1 localhost:5000/spring-argocd-app:v1
+#docker tag spring-argocd-app:v1 localhost:5000/spring-argocd-app:v1
 #docker login localhost:5000
-docker push localhost:5000/spring-argocd-app:v1
+#docker push localhost:5000/spring-argocd-app:v1
 #docker push chavanga/spring-argocd-app:v1
