4f841a3c18164730cf9df39eebbeda4b5628f770
Abel Chavanga <achavanga@bol.com>
2024-02-02 22:12:13 +0100

    Fix github package login issue

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 
-docker build -t spring-argocd-app:v1 .
+docker build -t ghcr.io/achavanga/springdemo/spring-argocd-app:v1 .
+
+docker push ghcr.io/achavanga/springdemo/spring-argocd-app:v1
 
 #docker tag spring-argocd-app:v1 localhost:5000/spring-argocd-app:v1
 #docker login localhost:5000
